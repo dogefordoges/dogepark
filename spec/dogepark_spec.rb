be550c23b8d63f2f7101d4c36f0e867f415976c5
hswick <hdswick@gmail.com>
2018-08-07 10:10:32 -0400

    Testing code of correct sign in

--- spec/dogepark_spec.rb
+++ spec/dogepark_spec.rb
@@ -6,7 +6,7 @@ def post(uri, json_data)
   req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
   req.body = json_data.to_json 
   http = Net::HTTP.new(uri.host, uri.port)
-  http.request(req).body
+  http.request(req)
 end
 
 RSpec.describe Net::HTTP, "Dogepark Server Tests" do
@@ -21,7 +21,7 @@ RSpec.describe Net::HTTP, "Dogepark Server Tests" do
       uri = URI(@base + "/signup")
       payload = {username: "hello", password: "world"}
       expected_response = {"message" => "signed up"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response
     end
 
     it "posts incorrect username" do
@@ -29,7 +29,7 @@ RSpec.describe Net::HTTP, "Dogepark Server Tests" do
       payload = {username: "hell0", password: "world"}
       post uri, payload
       expected_response = {"message" => "not signed up"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response      
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response      
     end    
 
     it "posts incorrect password" do
@@ -37,24 +37,15 @@ RSpec.describe Net::HTTP, "Dogepark Server Tests" do
       payload = {username: "hello", password: "world!"}
       post uri, payload
       expected_response = {"message" => "password incorrect"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response      
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response      
     end
 
     it "posts signed in user" do
       uri = URI(@base + "/signup")
       payload = {username: "hello", password: "world"}
-      post uri, payload
-      #expected_response = {"message" => "You have been signed up! Go ahead and sign in."}
-      #expect(JSON.parse(post uri, payload)).to eq expected_response      
+      expect(post(uri, payload).code).to eq "200"
     end    
 
-    it "posts signed in user" do
-      uri = URI(@base + "/signup")
-      payload = {username: "hello", password: "world"}
-      post uri, payload
-      #expected_response = {"message" => "You have been signed up! Go ahead and sign in."}
-      #expect(JSON.parse(post uri, payload)).to eq expected_response      
-    end
   end
 
   context "Dogepark interaction" do
@@ -69,35 +60,35 @@ RSpec.describe Net::HTTP, "Dogepark Server Tests" do
       uri = URI(@base + "/location")
       payload = {username: "hello", password: "world", latitude: "0.0", longitude: "0.0"}
       expected_response = {"message" => "Location saved!"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response
     end
 
     it "posts withdraw" do
       uri = URI(@base + "/withdraw")
       payload = {username: "hello", password: "world", address: @address, withdrawAddress: "0x1234567781223", amount: 100}
       expected_response = {"message" => "100 Ð was sent from your account to 0x1234567781223"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response
     end
 
     it "posts rain" do
       uri = URI(@base + "/rain")
       payload = {username: "hello", password: "world", address: @address, latitude: 0.0, longitude: 0.0, amount: 100, radius: 10}
       expected_response = {"message" => "You made it rain 100 Ð on 20 shibes in a 10 km radius around coordinate 0.0 lat, 0.0 long"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response
     end
 
     it "posts bowl" do
       uri = URI(@base + "/bowl")
       payload = {username: "hello", password: "world", address: @address, bowlAmount: 100, biteAmount: 1}
       expected_response = {"message" => "Here is your new bowl code: 0x123456. Total of 100 bites at 1 Ð a piece"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response
     end
 
     it "posts bite" do
       uri = URI(@base + "/bite")
       payload = {address: @address, bowlCode: "0x123456"}
       expected_response = {"message" => "You got a bite of 20 Ð!"}
-      expect(JSON.parse(post uri, payload)).to eq expected_response
+      expect(JSON.parse(post(uri, payload).body)).to eq expected_response
     end
 
     it "gets rain logs" do
