1aa246d340d07c864c115fc3ade5f94734dec833
hswick <hdswick@gmail.com>
2018-08-24 14:25:10 -0500

    Created script to initialize environment for demos

---
+++ demo.rb
@@ -0,0 +1,15 @@
+# Initialize environment for demo purposes
+
+require './database'
+
+db = Database.new
+
+db.drop_bowls
+db.drop_rain_logs
+db.drop_users
+
+db.create_users
+db.create_bowls
+db.create_rain_logs
+
+db.insert_user({name: "hello", password: "world", public_key: "bar", private_key: "foo"})
